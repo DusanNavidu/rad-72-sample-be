2458bd85fc13cff6dae90fb7466f877f744d8c69
Dusan Navidu <navidu200210@gmail.com>
2025-11-20 13:16:32 +0530

    fix: update CORS origin to allow requests from the production URL

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://rad-72-sample-be-one.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
